1940227d50aa058fa549bba3df5e9d40dd3f9193
rayatqoopio <ray.at.qoopio@gmail.com>
2025-10-29 22:54:21 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,11 @@
-self.addEventListener("install", (event) => {
-  self.skipWaiting();
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open('qoopio-cache').then((cache) => {
+    return cache.addAll(['/', '/index.html', '/manifest.json', '/icon.png']);
+  }));
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(fetch(event.request));
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
 });
